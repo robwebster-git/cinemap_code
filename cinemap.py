2919ff8855d790774c2017586d2f646803b41688
Jess Roberts <s1434165@baltic10.geos.ed.ac.uk>
2020-02-09 21:03:55 +0000

    Adding shops collapsible and some more styling2

--- cinemap.py
+++ cinemap.py
@@ -77,11 +77,11 @@ def foliumMap():
     for row in c:
         folium.Marker(row[1:],popup=row[0],icon=folium.Icon(color='red', icon='film')).add_to(map1)
 
-
+    """
     c.execute("SELECT B.STOP, B.GEOM.SDO_POINT.Y, B.GEOM.SDO_POINT.X FROM s1983906.BUS_STOPS B")
     for row in c:
         folium.Marker(row[1:],popup=row[0],icon=folium.Icon(color='green', icon='road')).add_to(map1)
-
+    """
 
     print('<h6>Query: '+str(sql+filter)+'</h6>')
     conn.close()
@@ -232,33 +232,6 @@ def facilitiesFilter():
 
     # return completed query filter
     return filter, chp
-
-
-def getDBdata(table_name='CINEMAS', order_column='CINEMA_ID'):
-    #  Accesses the Oracle database and creates Find, Field, Crop & MyClass
-    #  objects with which to create the website visualiser tools
-    results = []
-    with open('../../../details.txt', 'r') as f:
-        pwd = f.readline().strip()
-    try:
-        conn = cx_Oracle.connect(f"s0092179/{pwd}@geoslearn")
-        c = conn.cursor()
-        c.execute(f"SELECT * FROM {table_name} ORDER BY {order_column}")
-    except:
-        print("Failed to connect to Database Server...")
-
-    if table_name == "CINEMAS":
-        cinemas_list = []
-        for row in c:
-            (a, b, c, d, e) = row
-            cinema_name = table_name[:-1] + str(a)
-            cinema_name = Cinema(a, b, c, d, e)
-            cinemas_list.append(cinema_name)
-            results = cinemas_list
-    else:                                             #  if no table name matches are made, go to the else...
-        print("Table Name not supported...")
-    conn.close()                                      #  close the connection to the Oracle server
-    return results
 
 if __name__ == '__main__':
     print("Content-type: text/html\n")      #  Ensures that the webpage is rendered correctly using HTML code
