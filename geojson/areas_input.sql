d2b7a672492d7c6c6d07e36fda936f8e209cbd23
Rob Webster <s0092179@ed.ac.uk>
2020-02-14 18:13:44 +0000

    cleaning up local repo and removing uneccesary files from the git index

--- geojson/areas_input.sql
+++
@@ -1,4 +0,0 @@
-insert into areas values (2,'','
-
-'
-);
--- geojson/json_table.sql
+++
@@ -1,5 +0,0 @@
-create table json_example ( 
-id number primary key, 
-meadows_json clob, 
-constraint meadows_json check (meadows_json is JSON) 
-);
